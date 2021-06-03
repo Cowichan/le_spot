3bfddc13949bf200772b5707deb44df962ac62eb
cowichan <fourniercowichan@gmail.com>
2021-06-03 16:41:41 +0200

    go le Spot map

--- js/map.js
+++ js/map.js
@@ -10,5 +10,5 @@ var marker = L.marker(ga).addTo(mymap);
 
 // ajout d'un popup
 marker.bindPopup(
-  "<h3>Pizzeria le Spot à Guéthary</h3><p class='moins'>491 avenue du Général de Gaulle 64210 Guéthary<br /><a href='https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route=43.4245%2C-1.6092#map=14/43.4460/-1.5783'>Visualiser l'itinéraire ?</a></p>"
+  "<h3>Pizzeria le Spot à Guéthary</h3><p class='moins'>491 avenue du Général de Gaulle 64210 Guéthary<br /><a href='https://www.openstreetmap.org/directions?from=&to=43.4245%2C%20-1.6092&route=%3B43.4245%2C-1.6092'>Visualiser l'itinéraire ?</a></p>"
 );
